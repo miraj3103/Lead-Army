dae14768e85696e60d455f185a2d3a75f44d6151
miraj3103 <mdtufsirulmiraj494129@gmail.com>
2025-02-06 08:21:09 +0600

    theme changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    darkMode: false, // or 'media' or 'class'
-    content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
+  darkMode: false, // or 'media' or 'class'
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  daisyui: {
+    themes: ["light"], // Ensure only light theme is used
+  },
+  plugins: [],
+};
