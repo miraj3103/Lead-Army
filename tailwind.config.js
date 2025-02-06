67a9fb1efd00d0ade14dc3aeb92b84a0e14310f3
miraj3103 <mdtufsirulmiraj494129@gmail.com>
2025-02-06 07:36:42 +0600

    fix Image Album Position

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    darkMode: false, // or 'media' or 'class'
     content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
     theme: {
       extend: {},
